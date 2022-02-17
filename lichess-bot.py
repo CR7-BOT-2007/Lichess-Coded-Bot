482056f3e64702cc33a1a103af3310effd8d1f6a
2022FIFA <99064634+2022FIFA@users.noreply.github.com>
2022-02-17 09:20:12 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -233,11 +233,9 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
         def __init__(self, room):
             self.room = room
     opponent = game.black.name if game.white.name == user_profile["username"] else game.white.name
-    conversation.send_reply(SendLine('player'), f'Made by @Servan5635')
     conversation.send_reply(SendLine('player'), f'Good Luck {opponent}!')
-    conversation.send_reply(SendLine('player'), f'Type !help for list of commands I can Reply to.Thanks for playing with me')
-    conversation.send_reply(SendLine('spectator'), f'Made by @Servan5635')
-    conversation.send_reply(SendLine('spectator'), f'Thanks for spectating me!')
+    conversation.send_reply(SendLine('player'), f'Type !help for list of commands I can Reply to.Programmed by @SNIC_Yashen.Thanks for playing with me')
+    conversation.send_reply(SendLine('spectator'), f'Made by @SNIC_Yashen')
     conversation.send_reply(SendLine('spectator'), f'Type !help for list of commands I can reply to!')
     
     variant=game.perf_name
